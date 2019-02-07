48494903b32658ab63501649b1725f16b2a55bef
Sean DeBarr <setdebarr@gmail.com>
2019-02-06 21:14:36 -0500

    add better functionality to drive motor control

--- Test_Pololu_Dual_VNH5019_Motor_Driver/Test_Pololu_Dual_VNH5019_Motor_Driver.ino
+++ Test_Pololu_Dual_VNH5019_Motor_Driver/Test_Pololu_Dual_VNH5019_Motor_Driver.ino
@@ -2,17 +2,28 @@
 
 DualVNH5019MotorShield md;
 
+const int buttonPinF = A0;
+const int buttonPinB = A1;
+const int speedSensor = A2;
+const int eStop = A3;
+
+int buttonFState = 0;
+int buttonBState = 0;
+int speedValue = 0;
+int eStatus = 0;
+
 void stopIfFault()
 {
   if (md.getM1Fault())
   {
     Serial.println("M1 fault");
-    while(1);
+    delay(1000);
+    
   }
   if (md.getM2Fault())
   {
     Serial.println("M2 fault");
-    while(1);
+    delay(1000);
   }
 }
 
@@ -21,53 +32,39 @@ void setup()
   Serial.begin(115200);
   Serial.println("Dual VNH5019 Motor Shield");
   md.init();
+  pinMode(buttonPinF, INPUT);
+  pinMode(buttonPinB, INPUT);
+  pinMode(eStop, INPUT);
 }
 
 void loop()
 {
-  for (int i = 0; i <= 400; i++)
-  {
-    md.setM1Speed(i);
-    md.setM2Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M1 current: ");
-      Serial.println(md.getM1CurrentMilliamps());
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
-    }
-    delay(2);
-  }
+  eStatus = digitalRead(eStop);
+  buttonFState = digitalRead(buttonPinF);
+  buttonBState = digitalRead(buttonPinB);
+  speedValue = map(analogRead(speedSensor), 0, 1023 , 0, 400);
   
-  for (int i = 400; i >= -400; i--)
-  {
-    md.setM1Speed(i);
-    md.setM2Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M1 current: ");
-      Serial.println(md.getM1CurrentMilliamps());
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
-    }
-    delay(2);
-  }
-  
-  for (int i = -400; i <= 0; i++)
-  {
-    md.setM1Speed(i);
-    md.setM1Speed(i);
-    stopIfFault();
-    if (i%200 == 100)
-    {
-      Serial.print("M1 current: ");
-      Serial.println(md.getM1CurrentMilliamps());
-      Serial.print("M2 current: ");
-      Serial.println(md.getM2CurrentMilliamps());
-    }
-    delay(2);
-  }
+  Serial.print("M1 current: ");
+  Serial.println(md.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(md.getM2CurrentMilliamps());
+  Serial.println(speedValue);
 
+  if (eStatus == HIGH) {
+    md.setBrakes(400, 400);
+    stopIfFault();
+  }
+  if (buttonFState == HIGH) {
+    for(int i = 0 ;  i <= speedValue; i++){
+    md.setSpeeds(i, i);
+    stopIfFault();
+    }
+  }
+  if (buttonFState == HIGH) {
+    for(int i = 0 ;  i <= speedValue; i++){
+    md.setSpeeds(-i, -i);
+    stopIfFault();
+    }
+  }
+  
 }

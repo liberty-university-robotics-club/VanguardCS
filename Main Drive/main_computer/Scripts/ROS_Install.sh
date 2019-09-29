0c8d8622f02f10375bbcc30d835dcd9cfbff866f
Vanguard <vanguard@Vanguard>
2019-09-28 20:14:52 -0400

    Added ROS install script for debian

---
+++ Main Drive/main_computer/Scripts/ROS_Install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo Starting install ...
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-get install dirmngr --install-recommends
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get install ros-melodic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt update && apt upgrade -y
+source /opt/ros/melodic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+echo To make sure your workspace is properly overlayed by the setup script, make sure ROS_PACKAGE_PATH environment variable includes the directory you\'re in.
+echo $ROS_PACKAGE_PATH
+echo .
+echo Install finished

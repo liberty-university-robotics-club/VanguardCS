4561431a052b7b6eb31b79e4d46a5ca62d1ee2c8
Sean DeBarr <setdebarr@gmail.com>
2019-11-23 22:32:41 +0000

    added ROS workspace

---
+++ main_computer/catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_py"
+  "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/_Num.py"
+  "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/_AddTwoInts.py"
+  "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/__init__.py"
+  "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main_computer/catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/src/beginner_tutorials/msg/Num.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/vanguard/Desktop/VanguardCS/main_computer/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
